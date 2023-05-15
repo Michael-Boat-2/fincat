d9cacf7cac4627bdc32912b8f066569853f51455
Michael-Boateng <131008187+Michael-Boat-2@users.noreply.github.com>
2023-05-15 17:21:09 +0200

    Create ratio.h

---
+++ srcCode/ratio.h
@@ -0,0 +1,8 @@
+class caclRatio
+{
+private:
+public:
+    double calcRatioPM(double revenue, double expenses);
+    double calcRatioRoA(double revenue, double expenses, double assets);
+    double calcRatioDE(double assets, double liabilities);
+};
