e6028e8968bb94f81be49a6b922043a88d2d18ba
Michael-Boateng <131008187+Michael-Boat-2@users.noreply.github.com>
2023-05-15 17:24:31 +0200

    Create calcRatio.h

---
+++ srcCode/calcRatio.h
@@ -0,0 +1,14 @@
+class caclRatio
+{
+private:
+public:
+    double calcRatioPM(double revenue, double expenses){
+
+    };
+    double calcRatioRoA(double revenue, double expenses, double assets){
+
+    };
+    double calcRatioDE(double assets, double liabilities){
+
+    };
+};
