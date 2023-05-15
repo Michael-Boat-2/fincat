d902b5364a6f0efe04b49f713594f4a7e86a580d
Michael-Boateng <131008187+Michael-Boat-2@users.noreply.github.com>
2023-05-15 17:24:34 +0200

    Delete ratio.h

--- srcCode/ratio.h
+++
@@ -1,8 +0,0 @@
-class caclRatio
-{
-private:
-public:
-    double calcRatioPM(double revenue, double expenses);
-    double calcRatioRoA(double revenue, double expenses, double assets);
-    double calcRatioDE(double assets, double liabilities);
-};
